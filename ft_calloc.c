cb2bad0ccd239c3e2f7d0e8c55d6c30fd26f0af4
Rhyan Clark Randriamisaina <rhrandri@c1r8s16.42antananarivo.mg>
2026-01-21 09:21:18 +0300

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhrandri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 08:47:03 by rhrandri          #+#    #+#             */
+/*   Updated: 2026/01/21 09:19:16 by rhrandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*r;
+
+	r = malloc(count * size);
+	if (!r)
+		return (NULL);
+	ft_bzero(r, size * count);
+	return (r);
+}
