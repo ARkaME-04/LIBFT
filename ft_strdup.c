ce58c2f7be438f876f67544f5e1896a5df7d0dd3
Rhyan Clark Randriamisaina <rhrandri@c1r8s16.42antananarivo.mg>
2026-01-21 09:57:09 +0300

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhrandri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 09:53:53 by rhrandri          #+#    #+#             */
+/*   Updated: 2026/01/21 09:56:25 by rhrandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	size_t	len;
+
+	len = ft_strlen(s1) + 1;
+	s2 = malloc(len * sizeof(char));
+	if (!s2)
+		return (NULL);
+	ft_strlcpy(s2, s1, len);
+	return (s2);
+}
