639d813b8abeb9d7f84985e5a596f03a5657c4b3
Rhyan Clark Randriamisaina <rhrandri@c1r8s11.42antananarivo.mg>
2026-01-23 10:14:59 +0300

    cmp remade

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rhrandri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 10:40:21 by rhrandri          #+#    #+#             */
-/*   Updated: 2026/01/19 10:45:35 by rhrandri         ###   ########.fr       */
+/*   Updated: 2026/01/23 10:01:54 by rhrandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] != '\0' && s2[i] != '\0')
 		&& (i < n - 1) && s1[i] == s2[i])
 		i++;
+	if (i == n)
+		return (0);
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
