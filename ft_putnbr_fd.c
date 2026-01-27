b7406f9986a22cb824a9597b441cbe1cc7c58778
Rhyan Clark Randriamisaina <rhrandri@c1r1s12.42antananarivo.mg>
2026-01-27 11:15:04 +0300

    putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,11 @@
 /*   By: rhrandri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 15:40:21 by rhrandri          #+#    #+#             */
-/*   Updated: 2026/01/26 15:45:35 by rhrandri         ###   ########.fr       */
+/*   Updated: 2026/01/27 10:47:05 by rhrandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-include "libft.h"
+#include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
